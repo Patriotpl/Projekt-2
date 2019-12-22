4a2bc75b21f04ec15391bb860e0938b24143b8fc
Patriotdom <patriotpl22@gmail.com>
2019-12-22 19:07:05 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $('#content').load("text.html");
+    $('nav a').on('click',function () {
+        var url = $(this).attr("href");
+        $("#content").fadeOut(1000,function () {
+            $("#content").load(url,function () {
+                $("#content").fadeIn(1000);
+            });
+        });
+        return false;
+    });
+
+    $("a.rozszerz").on('click', function () {
+        $('.addition').appendTo(".jumbotron");
+    });
+
+    //$(function () {
+        //if ($("#Select").val() == "Dobry")
+           // $("#WSelect").prop('disabled', true);
+        //else
+            //$("#WSelect").disabled('disabled', false);
+    //})
+
+    $("a#single_image").fancybox();
+
+    /* Using custom settings */
+
+    $("a#inline").fancybox({
+        'hideOnContentClick': true
+    });
+
+    /* Apply fancybox to multiple items */
+
+    $("a.group").fancybox({
+        'transitionIn'	:	'elastic',
+        'transitionOut'	:	'elastic',
+        'speedIn'		:	600,
+        'speedOut'		:	200,
+        'overlayShow'	:	false
+    });
+
+    $('.navbar-toggler').on('click', function () {
+        if($('.navbar-collapse').hasClass('in'))
+        {
+            $('.navbar-collapse').removeClass('collapse');
+        }
+        else
+        {
+            $('.navbar-collapse').addClass('collapse');
+        }
+    });
+});
